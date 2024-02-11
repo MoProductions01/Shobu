e65cb8b906f22dedd199d54d7dc8f51ea1fb35e6
Mike ONeil <MoProductions01@gmail.com>
2024-02-11 12:28:59 -0700

    Updated rock control

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -7,75 +7,15 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Rock : MonoBehaviour//, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class Rock : MonoBehaviour
 {    
     public enum eRockColor {BLACK, WHITE};
     [field: SerializeField] public eRockColor RockColor {get; set;}   
-
-    int BoardSpaceMask;     
-    Board MyBoard;
+     
+    public Board MyBoard {get; set;}
 
     private void Start() 
     {
-        MyBoard = transform.parent.parent.parent.parent.GetComponent<Board>();
-        BoardSpaceMask = LayerMask.GetMask("Board Space");
+        MyBoard = GetComponentInParent<Board>();        
     }
-
-#if false
-    public void OnDrag(PointerEventData eventData)
-    {        
-        Vector3 screenToworld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(screenToworld.x, screenToworld.y, 0f);        
-    }
-
-    public void OnBeginDrag(PointerEventData eventData)
-    {        
-        transform.localScale *= 1.2f;
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {     
-        transform.localScale /= 1.2f;
-
-        Vector3 camPosition = Camera.main.transform.localPosition;
-        Vector3 mouseZ = Camera.main.WorldToScreenPoint(new Vector3(0, 0,Input.mousePosition.z));
-        Vector3 origin = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mouseZ.z));                       
-        RaycastHit2D hit2D =Physics2D.Raycast(new Vector2(origin.x, origin.y), Vector2.up, Mathf.Infinity, BoardSpaceMask);
-
-        if(hit2D.collider) 
-        {            
-            GameObject hitSpace = hit2D.collider.gameObject;            
-            string[] rowCol = hitSpace.name.Split(",");    
-            Vector2Int loc = new Vector2Int(Int32.Parse(rowCol[0]), Int32.Parse(rowCol[1]));
-           // Debug.Log("loc: " + loc.ToString());
-
-            
-            //Board myBoard = transform.parent.parent.parent.parent.GetComponent<Board>();
-            Board spaceBoard = hit2D.collider.transform.parent.parent.parent.GetComponent<Board>();
-            if(MyBoard == spaceBoard)
-            {
-                //Debug.Log("On correct board");
-                if(hitSpace.GetComponentInChildren<Rock>() != null)
-                {
-                //    Debug.Log("There's a rock on this space so don't put it here");                    
-                }
-                else
-                {
-                  //  Debug.Log("Space is free so place rock");
-                    transform.parent = hitSpace.transform;                    
-                }                                
-            }                       
-            else
-            {
-                //Debug.Log("Incorrect board");                
-            }
-            transform.localPosition = Vector3.zero;
-        } 
-        else 
-        {
-            //Debug.Log("Null Hit");
-            MyBoard.PutRockOnPushedList(this);            
-        }
-    }
-    #endif
 }
--- Assets/Scripts/Shobu.cs
+++ Assets/Scripts/Shobu.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,8 @@ public class Shobu : MonoBehaviour
     public static int NUM_BOARDS = 4;
                  
     [SerializeField] List<Board> Boards = new List<Board>();
-
-    int RockMask;
+    Rock HeldRock;
+    int RockMask, BoardSpaceMask;
 
 
     // Start is called before the first frame update
@@ -16,7 +17,10 @@ public class Shobu : MonoBehaviour
     {        
         ResetBoards();
         RockMask = LayerMask.GetMask("Rock");
+        BoardSpaceMask = LayerMask.GetMask("Board Space");
     }
+
+
 
     public void ResetBoards()
     {
@@ -31,19 +35,68 @@ public class Shobu : MonoBehaviour
         ResetBoards();
     }    
 
+    RaycastHit RayCast(int layerMask)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;       
+        Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask);
+        return hit;
+    }
+
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
-        {            
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            Vector3 origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);            
-            if(Physics.Raycast(ray, out hit, Mathf.Infinity, RockMask))
+        {                        
+            RaycastHit hit = RayCast(RockMask);
+            if(hit.collider != null)
             {
-                Rock hitRock = hit.collider.GetComponent<Rock>();
-                Board hitRockBoard = hitRock.GetComponentInParent<Board>();
-                Debug.Log("clicked on: " + hitRockBoard.name + " - " + hitRock.name);                
+                HeldRock = hit.collider.GetComponent<Rock>();
+                Board hitRockBoard = HeldRock.GetComponentInParent<Board>();
+                HeldRock.transform.localScale *= 1.2f;
+                Debug.Log("clicked on: " + hitRockBoard.name + " - " + HeldRock.name);                
             }            
+        }
+        else if(Input.GetMouseButton(0) && HeldRock != null)
+        {
+            Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            HeldRock.transform.position = new Vector3(mouseWorld.x, mouseWorld.y, 0f);
+        }
+        else if(Input.GetMouseButtonUp(0) && HeldRock != null)
+        {
+            RaycastHit hit = RayCast(BoardSpaceMask);
+            if(hit.collider != null)
+            {
+                //Debug.Log("let go over: " + hit.collider.name);
+                BoardSpace hitBoardSpace = hit.collider.GetComponent<BoardSpace>();
+                Board hitBoardSpaceBoard = hitBoardSpace.GetComponentInParent<Board>();
+                //Debug.Log("released on BoardSpace: " + hitBoardSpaceBoard.name + " - " + hitBoardSpace.name);  
+               // Debug.Log("HeldRock Board: " + HeldRock.MyBoard.name + ", BoardSpace Board: " + hitBoardSpaceBoard.name);     
+                if(HeldRock.MyBoard == hitBoardSpaceBoard)
+                {
+                  //  Debug.Log("Let go on correct board");
+                    if(hitBoardSpace.GetComponentInChildren<Rock>() != null)
+                    {
+                   //     Debug.Log("There's a rock on this space so don't put it here");
+                    }
+                    else
+                    {
+                     //   Debug.Log("Space is free so place rock");
+                        HeldRock.transform.parent = hitBoardSpace.transform;
+                    }
+                }
+                else
+                {
+                   // Debug.Log("Incorrect Board");
+                }
+                HeldRock.transform.localPosition = Vector3.zero;
+            }            
+            else
+            {
+               // Debug.Log("Released over a non BoardSpace");
+                HeldRock.MyBoard.PutRockOnPushedList(HeldRock);        
+            }   
+            HeldRock.transform.localScale /= 1.2f;         
+            HeldRock = null;
         }
     }
 }

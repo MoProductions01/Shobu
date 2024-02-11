2e6647a7df9485cbd8793d6e2255e5083d68cfda
Mike ONeil <MoProductions01@gmail.com>
2024-02-10 18:35:34 -0700

    First pass at rock movement input

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Rock : MonoBehaviour
+public class Rock : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    public enum eRockColor {LIGHT, DARK};
+    public enum eRockColor {DARK, LIGHT};
 
-    [SerializeField] private eRockColor RockColor {get; set;}
+    [field: SerializeField] private eRockColor RockColor {get; set;}
     
+   // private RectTransform RectTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //RectTransform = GetComponent<RectTransform>();
+    }    
+
+    public void OnDrag(PointerEventData eventData)
+    {        
+        Vector3 screenToworld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(screenToworld.x, screenToworld.y, 0f);        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        Debug.Log("OnBeginDrag: " + this.name);        
+        transform.localScale *= 1.2f;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag: " + this.name);
+        transform.localScale /= 1.2f;
     }
 }
